f34a60df0002c2700214daea551092f3ebde7c51
Burhan Mafazi <burhanburdev@gmail.com>
2022-05-07 20:08:24 +0700

    add .env.example

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.auth, name='auth'),
+    path('login', views.auth, name='auth'),
     path('', views.index, name='index'),
     path('<int:id>', views.show, name='show'),
 ]
